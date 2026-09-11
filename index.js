6593735da2eecc029ea624a42cef00e5c66dad02
Mufassir Mahmud <mufassir389@gmail.com>
2026-09-11 21:30:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
