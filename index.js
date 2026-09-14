8b053cf1f26a7a9dd275c22843f7f5c2c105dc24
Mufassir Mahmud <mufassir389@gmail.com>
2026-09-14 19:44:25 +0600

    added payment strip system

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 // middleware
 app.use(express.json())
 app.use(cors())
@@ -30,20 +31,70 @@ async function run() {
         if(email){
             query.SenderEmail = email
         }
-        const cursor = parcelsCollection.find();
+        const options = {sort : {createdAt : -1}}
+        const cursor = parcelsCollection.find(query,options);
         const result = await cursor.toArray();
         res.send(result)
     })
-
+    app.get('/parcels/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await parcelsCollection.findOne(query);
+      res.send(result)
+    })
     app.post('/parcels', async(req,res)=>{
         const parcel = req.body;
+        parcel.createdAt = new Date()
         const result = await parcelsCollection.insertOne(parcel);
         res.send(result)
     })
 
 
+    app.delete('/parcels/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await parcelsCollection.deleteOne(query)
+      res.send(result)
+    })
 
+    // STRIPE Payment Methood
 
+    app.post('/create-checkout-session', async(req,res)=>{
+      const paymentInfo = req.body
+      const amount = parseInt(paymentInfo.cost )* 100; 
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+      {
+        // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+        // price_data: {
+        //   currency: 'USD',
+        //   unit_amount: 1500,
+        //   product_data: {
+        //     name: paymentInfo.parcelName
+        //   }
+        // },
+        price_data: {
+          currency: 'USD',
+          unit_amount: amount,
+          product_data: {
+            name: paymentInfo.parcelName
+          }
+        },
+        quantity: 1,
+      },
+    ],
+    customer_email: paymentInfo.senderEmail,
+    mode: 'payment',
+    metadata: {
+      parcelId: paymentInfo.parcelId
+    },
+    success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+    cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+  
+      })
+      console.log(session)
+      res.send({url : session.url})
+    })
 
 
     // Send a ping to confirm a successful connection
